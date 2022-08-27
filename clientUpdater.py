8dc392d7ddde53adc454c52265e4d5afbbde23d6
Nicholas Chavez <NChavez0516@gmail.com>
2022-08-26 20:35:42 -0400

    upd

--- clientUpdater.py
+++ clientUpdater.py
@@ -1,9 +1,15 @@
 import os
 import requests
 
-def checkForNewVersion():
-    i = 1
+def getLatestCommit():
+    r = requests.get("https://github.com/NChavezOne/Roblox-Spam-Bot/commit/main")
+    response = str(r.text)
+    indexOf = (response.find(r"""class="sha user-select-contain">""") + 32)
+    subStr = response[indexOf:len(response)]
+    indexOf = (subStr.find(r"<"))
+    subStr = subStr[0:indexOf]
+    return subStr
     
 if __name__ == "__main__":
-    r = requests.get("https://github.com/NChavezOne/Roblox-Spam-Bot/commit/main")
-    print(r.text)
+    
+    print(getLatestCommit())
