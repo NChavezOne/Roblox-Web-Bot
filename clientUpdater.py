edc7cf52e0376cd3cdfb18c0593c4d8972514bb6
Nicholas Chavez <NChavez0516@gmail.com>
2022-08-26 20:06:34 -0400

    Added client updater

---
+++ clientUpdater.py
@@ -0,0 +1,9 @@
+import os
+import requests
+
+def checkForNewVersion():
+    i = 1
+    
+if __name__ == "__main__":
+    r = requests.get("https://github.com/NChavezOne/Roblox-Spam-Bot/commit/main")
+    print(r.text)
